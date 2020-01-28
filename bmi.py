860573707072c04b254543b359cbce887976297a
rastislp <g00252861@gmit.ie>
2020-01-28 10:38:26 +0000

    bmi calculator added

---
+++ bmi.py
@@ -0,0 +1,12 @@
+# week 2 
+# program to calculate bmi
+print()
+print("Welcome in your personal bmi calculator.")
+print()
+
+height = float(input ("Please enter your hight: "))
+weight = float(input ("Please enter your weight: "))
+
+bmi=weight/(height*height)
+print("Calculated bmi is",bmi,".")
+print()
