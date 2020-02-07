99f9c84feb47e132ca6693d44109126f3d81440c
rastislp <g00252861@gmit.ie>
2020-02-07 20:31:18 +0000

    correct answer to your question

--- collatz.py
+++ collatz.py
@@ -6,17 +6,34 @@
 #  if it is even, divide it by two, but if it is odd, multiply it by three
 #  and add one. Have the program end if the current value is one.
 
+#number= int(input ("Please choose your posiive number:  "))
+
+#while(number != 1):
+#    if (number % 2) == 0 and number>0: #if number modelo 2 = 0 and greater than 0
+#        x=number/2
+#        print("Answer is: ",x)
+#       break
+#    elif(number %2) ==1 and number>0: #if number modelo 2 = 1 and greater than 0
+#        x= (number*3) +1
+#        print("Answer is: ",x)
+#        break
+#   else:
+#        number <0
+#        print("This is not positive number")  
+#        break
+#print()     
+#print("Program Terminated")      
+
 number= int(input ("Please choose your posiive number:  "))
+while(number > 1):
+    if (number % 2) == 0:
+        print(number," ")
+        number=number/2
+        
+    else : 
+       print(number)
+       number= (number*3)+1      
+print(1.0)        
 
-
-if (number % 2) == 0 and number>0: #if number modelo 2 = 0 and greater than 0
-    x=number/2
-    print("Answer is: ",x)
-elif(number %2) ==1 and number>0: #if number modelo 2 = 1 and greater than 0
-    x= (number*3) +1
-    print("Answer is: ",x)
-else:
-    number <0
-    print("This is not positive number")  
-print()     
-print("Program Terminated")      
+           
+#took me while as imissread but finallygot it
