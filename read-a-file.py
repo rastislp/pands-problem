c35863416267922a55e24d60fa7dc9abc8ff6cc3
rastislp <g00252861@gmit.ie>
2020-03-06 21:58:18 +0000

    week 7 update comments

--- read-a-file.py
+++ read-a-file.py
@@ -1,8 +1,7 @@
 # Rastislav Petras
 # 06 March 2020
-# Excercise 6 
-# Write a program that reads in a text file and outputs the number of e's it contains. 
-# The program should take the filename from an argument on the command line.
+# week 7
+# open file
 
 with open('.gitignore', 'r') as f:
     #print(f.readline(), end=" ")
