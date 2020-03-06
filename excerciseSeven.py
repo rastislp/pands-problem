49ce0b56152101d8e27ea2d7c509cf235bcf1389
rastislp <g00252861@gmit.ie>
2020-03-06 23:26:53 +0000

    Excercise 7 completed

--- excerciseSeven.py
+++ excerciseSeven.py
@@ -1,19 +1,21 @@
 # Rastislav Petras
 # 06 March 2020
 # Excercise 7 
-# Write a program that reads in a text file and outputs the number of e's it contains. 
+# Write a program that reads in a text file and outputs the number of i's it contains. 
 # The program should take the filename from an argument on the command line.
 
-#This is a few words of my  new file john23
+#This is a few words of my  new file john23 IIIiI
 
-with open('myfile.txt', 'r') as f:
-    test_str = f.readline()
+yourFile = (input ("Please enter defined location of your file:\n   for example: myfile.txt  "))
+
+with open(str(yourFile), 'r') as f:
+    test_str = f.read()
 
     count = 0
     for i in test_str: 
-        if i == 'i' or 'I': 
+        if i == 'i' : 
             count = count + 1
   
 # printing result  
-count= count-40
+
 print ("Count is : " +  str(count))     
